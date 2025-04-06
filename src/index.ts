59d75944c14a3557d49440b4b27aee59c9d03fdb
Felipe Pizzorno <felipepizzorno98@gmail.com>
2025-04-05 23:08:29 -0300

    Kind of working first geometries

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,100 @@
 import { greet } from "./import"
-import * as THREE from 'three';
+import * as THREE from "three";
+import { startCameraAndControls } from "./three/camera"
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const main = () => {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 5;
 
-const animate = () => {
-  renderer.render( scene, camera );
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color( 'black' )
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  const {camera, controls} = startCameraAndControls(renderer.domElement);
+
+{
+
+		const planeSize = 40;
+
+		const loader = new THREE.TextureLoader();
+		const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.magFilter = THREE.NearestFilter;
+		texture.colorSpace = THREE.SRGBColorSpace;
+		const repeats = planeSize / 2;
+		texture.repeat.set( repeats, repeats );
+
+		const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+		const planeMat = new THREE.MeshPhongMaterial( {
+			map: texture,
+			side: THREE.DoubleSide,
+		} );
+		const mesh = new THREE.Mesh( planeGeo, planeMat );
+		mesh.rotation.x = Math.PI * - .5;
+		scene.add( mesh );
+
+	}
+
+	{
+
+		const cubeSize = 4;
+		const cubeGeo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
+		const cubeMat = new THREE.MeshPhongMaterial( { color: '#8AC' } );
+		const mesh = new THREE.Mesh( cubeGeo, cubeMat );
+		mesh.position.set( cubeSize + 1, cubeSize / 2, 0 );
+		scene.add( mesh );
+
+	}
+
+	{
+
+		const sphereRadius = 3;
+		const sphereWidthDivisions = 32;
+		const sphereHeightDivisions = 16;
+		const sphereGeo = new THREE.SphereGeometry( sphereRadius, sphereWidthDivisions, sphereHeightDivisions );
+		const sphereMat = new THREE.MeshPhongMaterial( { color: '#CA8' } );
+		const mesh = new THREE.Mesh( sphereGeo, sphereMat );
+		mesh.position.set( - sphereRadius - 1, sphereRadius + 2, 0 );
+		scene.add( mesh );
+
+	}
+
+	{
+
+		const color = 0xFFFFFF;
+		const intensity = 1;
+		const light = new THREE.AmbientLight( color, intensity );
+		scene.add( light );
+
+	}
+
+	const resizeRendererToDisplaySize = (renderer: THREE.WebGLRenderer) => {
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if ( needResize ) {
+			renderer.setSize( width, height, false );
+		}
+
+		return needResize;
+	}
+
+	function render() {
+		if ( resizeRendererToDisplaySize( renderer ) ) {
+			const canvas = renderer.domElement;
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
+		}
+
+		renderer.render( scene, camera );
+		requestAnimationFrame( render );
+	}
+
+	requestAnimationFrame(render);
 }
-renderer.setAnimationLoop( animate );
 
-console.log(greet('Macaco'))
+main()
---
+++ src/three/camera.ts
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+export const startCameraAndControls = (canvas: HTMLCanvasElement) => {
+  const fov = 45;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+  camera.position.set( 0, 10, 20 );
+
+  const controls = new OrbitControls( camera, canvas );
+  controls.target.set( 0, 5, 0 );
+  controls.update();
+  return {camera, controls}
+}
