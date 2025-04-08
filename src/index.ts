d704e3dec1c238c88cc66183e31de3f9bdebfb07
Felipe Pizzorno <felipe.pizzorno.ctr@one.app>
2025-04-08 00:16:17 -0300

    Device pixel ratio

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const main = async () => {
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
 
